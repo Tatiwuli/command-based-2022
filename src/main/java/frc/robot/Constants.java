8aeb0b5c85a480c61d98e2b73a82861157623364
Tatiane Wu Li <innywuli@gmail.com>
2022-02-14 14:06:44 -0300

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+public final class Constants {
+
+    public static final int km_left1Port = 0;
+    public static final int km_left2Port = 0;
+    public static final int km_right1Port = 0;
+    public static final int km_right2Port = 0;
+    public static final int kright2Port = 0;
+    public static final int kleft2Port = 0;
+    public static final int kright1Port = 0;
+    public static final int kleft1Port = 0;
+    public static final double kleftSpeed = 0;
+    public static final int kIntakePort = 0;}
